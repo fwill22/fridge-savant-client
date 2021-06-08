9c719a3fb25b91475ef15256c7505c030d7d7343
Matt <mattdawson18@gmail.com>
2021-06-08 15:54:00 +0100

    Basic hardcoded api working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 
 function App() {
+
+  const ingredientRequest = async (ingredient) => {
+    let result = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&ingredients=${ingredient}`)
+    let json = await result.json()
+    console.log(json)
+  }
   return (
     <div className='App'>
       <h1>Hello</h1>
+      <button onClick={() => ingredientRequest('apple')}>Give me apples</button>
     </div>
   );
+
 }
 
 export default App;
