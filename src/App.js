d1a3f6233c1191ff3872018f6fade819fc06bf14
Matt <mattdawson18@gmail.com>
2021-06-08 17:00:21 +0100

    Refactored for better SRP

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ function App() {
   const ingredientRequest = async (ingredient) => {
     let result = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&ingredients=${ingredient}`)
     let json = await result.json()
-    console.log(result)
+    // console.log(searchBasket)
     console.log(json)
   }
-
 
   const [ingredient, setIngredient] = useState(null)
 
@@ -18,14 +17,22 @@ function App() {
     setIngredient(e.target.value)
   }
 
+  const resetQuery = () => {
+    document.querySelector('#ingredientInput').value = ''
+  }
+
+  const searchRecipes = () => {
+    searchBasket.push(ingredient)
+    ingredientRequest(searchBasket.join(',+'))
+    resetQuery()
+  }
+
   return (
     <div className='App'>
       <h1>Hello</h1>
       <input type='text' id='ingredientInput' onChange={handleChange}></input>
       <button onClick={() => {
-        searchBasket.push(ingredient)
-        ingredientRequest(searchBasket.join(',+'))
-        document.querySelector('#ingredientInput').value = ''
+        searchRecipes()
       }}>Give me food</button>
     </div>
   );
