5fe1fa7b2ee3f0ebec662e225d7bc221f75539a8
Matt <mattdawson18@gmail.com>
2021-06-08 16:44:23 +0100

    Basic multi input search working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 
+const searchBasket = []
+
 function App() {
 
   const ingredientRequest = async (ingredient) => {
     let result = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&ingredients=${ingredient}`)
     let json = await result.json()
+    console.log(result)
     console.log(json)
   }
+
 
   const [ingredient, setIngredient] = useState(null)
 
@@ -17,11 +21,14 @@ function App() {
   return (
     <div className='App'>
       <h1>Hello</h1>
-      <input type='text' onChange={handleChange}></input>
-      <button onClick={() => ingredientRequest(ingredient)}>Give me food</button>
+      <input type='text' id='ingredientInput' onChange={handleChange}></input>
+      <button onClick={() => {
+        searchBasket.push(ingredient)
+        ingredientRequest(searchBasket.join(',+'))
+        document.querySelector('#ingredientInput').value = ''
+      }}>Give me food</button>
     </div>
   );
-
 }
 
 export default App;
