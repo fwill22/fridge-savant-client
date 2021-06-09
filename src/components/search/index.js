a705f33ce57a1a4eaf5ef2b1ec5ac877e48a38c6
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-06-09 12:51:36 +0100

    Added clear ingredients button - conditionally appears if basket populated

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import IngredientList from '../ingredientList'; 
 
 const SearchComponent = () => {
 
   const [basket, setBasket] = useState([])
+  
+  useEffect(() => {
+    if (basket.length !== 0) {
+      document.getElementById('reset-basket-button').style.display = "block"
+    } else {
+      document.getElementById('reset-basket-button').style.display = "none"
+    }
+  })
 
   const ingredientRequest = async (ingredient) => {
     let result = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&ingredients=${ingredient}`)
     let json = await result.json()
     console.log(json, basket)
-    resetBasket() 
   }
 
   const addIngredient = () => {
@@ -37,6 +44,7 @@ const SearchComponent = () => {
       searchRecipes()
     }}>Give me food</button>
       <IngredientList basket={basket} />
+      <button id='reset-basket-button' onClick={resetBasket}>Clear Ingredients</button>
     </div>
   )
 }
