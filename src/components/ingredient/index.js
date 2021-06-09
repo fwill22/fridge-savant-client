19d76ce8317fcd0ff018fb0a7ffd85fde9e7410a
Kane <kgin888@gmail.com>
2021-06-09 16:41:38 +0100

    added bin to delete ingredient

--- src/components/ingredient/index.js
+++ src/components/ingredient/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './index.css'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 const Ingredient = ({ name, deleteIngredient }) => {
   return (
     <div className="ingredient">
       <span className="ingredient-name">{name.charAt(0).toUpperCase() + name.slice(1)}</span>
-      <button onClick={() => deleteIngredient(name)}>Delete Ingredient</button>
+      <FontAwesomeIcon icon={faTrashAlt} onClick={() => deleteIngredient(name)} swapOpacity/>
     </div>
   );
 };
 
 export default Ingredient;
+
