c542109ff556b78c0cfc252dd9ab70cdf2aa5081
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-06-09 21:50:37 +0100

    bugToFix: infinite fetch loop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
 import React, { useState } from 'react';
-import SearchComponent from './components/search'
+import RecipeList from './components/RecipeList';
 
 function App() {
+  const [basket, setBasket] = useState([]);
+  const [mealData, setMealData] = useState(null);
 
+  // const ingredientRequest = async (ingredient) => {
+  //   let result = await fetch(
+  //     `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&ingredients=${ingredient}`
+  //   );
+  //   let json = await result.json();
+  //   console.log(json, basket);
+
+  //   // this is temporary
+  //   resetBasket();
+  // };
+
+  function getMeals(ingredients) {
+    fetch(
+      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&ingredients=${ingredients}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMealData(data[0].id);
+        console.log(mealData);
+      })
+      .catch(() => {
+        console.log('Error');
+      });
+    resetBasket();
+  }
+
+  function handleChange() {
+    basket.push(document.querySelector('#ingredient-input').value);
+    console.log(basket);
+    resetQuery();
+  }
+
+  function resetQuery() {
+    document.querySelector('#ingredient-input').value = '';
+  }
+
+  function resetBasket() {
+    setBasket([]);
+  }
+
+  function searchRecipes() {
+    getMeals(basket.join(',+'));
+  }
 
   return (
-    <div className='App'>
-      <h1>Hello</h1>
-      <SearchComponent/>
+    <div>
+      <input type='text' id='ingredient-input'></input>
+      <button onClick={handleChange}>Add ingredient</button>
+      <button
+        onClick={() => {
+          searchRecipes();
+        }}
+      >
+        Give me food
+      </button>
+      {mealData && <RecipeList mealData={mealData} />}
     </div>
   );
 }
--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,42 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const SearchComponent = () => {
-
-  const [basket, setBasket] = useState([])
+  const [basket, setBasket] = useState([]);
 
   const ingredientRequest = async (ingredient) => {
-    let result = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&ingredients=${ingredient}`)
-    let json = await result.json()
-    console.log(json, basket)
-    resetBasket() 
-  }
+    let result = await fetch(
+      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&ingredients=${ingredient}`
+    );
+    let json = await result.json();
+    console.log(json, basket);
 
-  const addIngredient = () => {
-    setBasket(basket.concat(document.querySelector('#ingredientInput').value))
-    resetQuery()
-  }
+    // this is temporary
+    resetBasket();
+  };
+
+  const handleChange = () => {
+    setBasket(basket.concat(document.querySelector('#ingredient-input').value));
+    resetQuery();
+  };
 
   const resetQuery = () => {
-    document.querySelector('#ingredientInput').value = ''
-  }
+    document.querySelector('#ingredient-input').value = '';
+  };
 
   const resetBasket = () => {
-    setBasket([])
-  }
+    setBasket([]);
+  };
 
   const searchRecipes = () => {
-    ingredientRequest(basket.join(',+'))
-  }
+    ingredientRequest(basket.join(',+'));
+  };
 
   return (
     <div>
-      <input type='text' id='ingredientInput'></input>
-      <button onClick={addIngredient}>Add ingredient</button>
-      <button onClick={() => {
-      searchRecipes()
-    }}>Give me food</button>
+      <input type='text' id='ingredient-input'></input>
+      <button onClick={handleChange}>Add ingredient</button>
+      <button
+        onClick={() => {
+          searchRecipes();
+        }}
+      >
+        Give me food
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default SearchComponent
+export default SearchComponent;
