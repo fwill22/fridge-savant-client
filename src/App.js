01e0a54b9abfddce80bd1619c9f7b0a352a71f86
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-06-09 15:55:06 +0100

    bugToFix: API ID fetch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
 import React, { useState } from 'react';
-import SearchComponent from './components/Search'
-// import RecipeComponent from './components/Recipe'
+import RecipeComponent from './components/Recipe';
 
 function App() {
+  const [basket, setBasket] = useState([]);
+  const [recipeIds, setRecipeIds] = useState('');
 
+  const ingredientRequest = async (ingredient) => {
+    let result = await fetch(
+      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&ingredients=${ingredient}`
+    );
+    let json = await result.json();
+    console.log(json);
+    setRecipeIds(recipeIds.concat(formatRecipeIds(json)));
+  };
+
+  const addIngredient = () => {
+    setBasket(basket.concat(document.querySelector('#ingredientInput').value));
+    resetQuery();
+  };
+
+  const resetQuery = () => {
+    document.querySelector('#ingredientInput').value = '';
+  };
+
+  const resetBasket = () => {
+    setBasket([]);
+  };
+
+  const resetRecipes = () => {
+    setRecipeIds('');
+  };
+
+  const searchRecipes = () => {
+    ingredientRequest(basket.join(',+'));
+  };
+
+  const formatRecipeIds = (data) => {
+    return data.map((data) => data.id).join(',');
+  };
 
   return (
-    <div className='App'>
-      <h1>Hello</h1>
-      <SearchComponent/>
-      
+    <div>
+      <h1>Fridge Savant</h1>
+      <input type='text' id='ingredientInput'></input>
+      <button onClick={addIngredient}>Add ingredient</button>
+      <button
+        onClick={() => {
+          searchRecipes();
+        }}
+      >
+        Give me food
+      </button>
+      <button onClick={(resetBasket, resetRecipes)}>Clear search</button>
+      {recipeIds.length !== 0 && <RecipeComponent recipeIds={recipeIds} />}
     </div>
   );
 }
--- src/components/Recipe/index.js
+++ src/components/Recipe/index.js
@@ -1,22 +1,29 @@
-import React, { useState } from 'react'
-import SearchComponent from '../Search'
+import React, { useState } from 'react';
 
-const RecipeComponent = (json) => {
-  
-  let recipe = json[0]
-  console.log(recipe)
+const RecipeComponent = ({ recipeIds }) => {
+  const [recipeData, setRecipeData] = useState([]);
+
+  if (recipeIds.length !== 0) {
+    fetch(
+      `https://api.spoonacular.com/recipes/informationBulk?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&ids=${recipeIds}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setRecipeData(data);
+        // console.log(recipeData);
+      })
+      .catch(() => {
+        console.log('error');
+      });
+  }
 
   return (
     <div>
       <ul>
-        <li> {recipe.title}</li>
-        <li> {recipe.image}</li>
-
+        <li> =</li>
       </ul>
     </div>
-  )
+  );
+};
 
-}
-
-
-export default RecipeComponent
+export default RecipeComponent;
