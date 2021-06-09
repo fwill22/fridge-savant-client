1f7f75f7e7ef976181c87e7791b6b7803547ccd4
Matt <mattdawson18@gmail.com>
2021-06-09 16:10:48 +0100

    Fixed infinite API loop

--- src/components/Recipe/index.js
+++ src/components/Recipe/index.js
@@ -3,19 +3,14 @@ import React, { useState } from 'react';
 const RecipeComponent = ({ recipeIds }) => {
   const [recipeData, setRecipeData] = useState([]);
 
-  if (recipeIds.length !== 0) {
-    fetch(
-      `https://api.spoonacular.com/recipes/informationBulk?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&ids=${recipeIds}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setRecipeData(data);
-        // console.log(recipeData);
-      })
-      .catch(() => {
-        console.log('error');
-      });
-  }
+ 
+  console.log(recipeIds)
+  fetch( `https://api.spoonacular.com/recipes/informationBulk?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&ids=${recipeIds}` )
+    .then((response) => response.json())
+    .then((data) => { 
+      console.log(data)
+    })
+ 
 
   return (
     <div>
