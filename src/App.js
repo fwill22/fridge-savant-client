30c3ace1b0d370c5a583c0869758a5b40a83a337
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-06-10 15:26:13 +0100

    Recipe and ingredients append to page

--- src/App.js
+++ src/App.js
@@ -1,62 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MealList from './components/MealList';
+import IngredientList from './components/IngredientList';
 
 const App = () => {
   const [basket, setBasket] = useState([]);
   const [mealData, setMealData] = useState(null);
   const [mealIds, setMealIds] = useState([]);
 
-  const getMeals = (ingredients) => {
+  useEffect(() => {
+    if (basket.length !== 0) {
+      document.getElementById('reset-basket-button').style.display = 'block';
+    } else {
+      document.getElementById('reset-basket-button').style.display = 'none';
+    }
+  });
+
+  const addIngredient = () => {
+    const newIngredient = document
+      .querySelector('#ingredient-input')
+      .value.toLowerCase();
+    if (basket.find((ingredient) => ingredient.name === newIngredient)) {
+      // add flash error message
+    } else {
+      setBasket(basket.concat({ name: newIngredient }));
+    }
+    resetQuery();
+  };
+
+  const deleteIngredient = (ingredientName) => {
+    setBasket(
+      basket.filter((ingredient) => ingredient.name !== ingredientName)
+    );
+  };
+
+  const getMealInfo = (ingredients) => {
     fetch(
-      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY3}&ingredients=${ingredients}`
+      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&ingredients=${ingredients}`
     )
       .then((response) => response.json())
       .then((data) => {
         setMealIds(mealIds.push(formatMealIds(data)));
-        mealSearch(mealIds);
+        getMealData(mealIds);
       })
       .catch(() => {
-        console.log('Error');
+        ('Error');
       });
-  }
+  };
 
-  const mealSearch = (mealIds) => {
-    console.log(mealIds);
+  const getMealData = (mealIds) => {
     let mealIdString = mealIds.join();
     fetch(
-      `https://api.spoonacular.com/recipes/informationBulk?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY3}&ids=${mealIdString}`
+      `https://api.spoonacular.com/recipes/informationBulk?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&ids=${mealIdString}`
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         setMealData(data);
         resetMealIds();
       });
   };
 
-
-  const handleChange = () => {
-    basket.push(document.querySelector('#ingredient-input').value);
-    console.log(basket);
-    resetQuery();
-  }
-
   const resetQuery = () => {
     document.querySelector('#ingredient-input').value = '';
-  }
+  };
 
   const resetBasket = () => {
     setBasket([]);
-  }
+  };
 
   const searchMeals = () => {
-    getMeals(basket.join(',+'));
-  }
+    const ingredientNames = basket.map((ingredient) => ingredient.name);
+    getMealInfo(ingredientNames.join(',+'));
+  };
 
   const formatMealIds = (data) => {
     return data.map((data) => data.id).join(',');
   };
-
 
   const resetMealIds = () => {
     setMealIds([]);
@@ -64,24 +82,31 @@ const App = () => {
 
   const resetMeals = () => {
     setMealData(null);
-  }
+  };
 
   const clearAll = () => {
-    resetBasket()
-    resetMeals()
-  }
+    resetBasket();
+    resetMeals();
+  };
 
   return (
     <div>
       <input type='text' id='ingredient-input'></input>
-      <button onClick={handleChange}>Add ingredient</button>
-      <button onClick={() => { searchMeals() }}>
+      <button onClick={addIngredient}>Add ingredient</button>
+      <button
+        onClick={() => {
+          searchMeals();
+        }}
+      >
         Give me food
       </button>
-      <button id='reset-basket-button' onClick={clearAll}>Clear Ingredients</button>
+      <button id='reset-basket-button' onClick={clearAll}>
+        Clear Ingredients
+      </button>
+      <IngredientList basket={basket} deleteIngredient={deleteIngredient} />
       {mealData && <MealList mealData={mealData} />}
     </div>
   );
-}
+};
 
 export default App;
