9b60002e4aa9d71d458f0d627b88310497576947
Matt <mattdawson18@gmail.com>
2021-06-10 11:36:25 +0100

    Source urls returned so users can find detailed recipes

--- src/components/Meal/index.js
+++ src/components/Meal/index.js
@@ -3,7 +3,11 @@ import React, { useEffect } from 'react';
 const Meal = ({ meal }) => {
   
 
-  return <h1>{meal.title}</h1>;
+  return (
+    <div>
+     <h1><a href={meal.sourceUrl}>{meal.title}</a></h1>
+    </div>
+  )
 };
 
 export default Meal;
