bb705ef7bf0356e82c293093852a3863f40b0c22
MattDawson2020 <78559243+MattDawson2020@users.noreply.github.com>
2021-06-14 10:58:58 +0100

    Update index.js

--- src/components/MealList/index.js
+++ src/components/MealList/index.js
@@ -7,8 +7,7 @@ const MealList = ({ mealData }) => {
     <section class='Recipes'>
       <h1 class='test'>Recipes for you</h1>
       {mealData.map((meal) => {
-        return <Meal key={meal.summary} meal={meal} />;
-        // tests want a unique key visible within html, can change back to id for better unique key
+        return <Meal key={meal.id} meal={meal} />;
       })}
     </section>
   );
