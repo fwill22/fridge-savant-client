4690d7c9c206f7d13545b040bd10fdd275a86c34
Kane <kgin888@gmail.com>
2021-06-15 19:09:25 +0100

    styled flash messages - basic with transition in

--- src/components/FlashMessage/index.js
+++ src/components/FlashMessage/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
 const FlashMessage = ({type, message, setFlashMessage }) => {
+
+  const [outro, setOutro] = useState(0)
+
   return (
-    <div className="flash-message" onClick={() => setFlashMessage(null)}>
-      <p className={type}>{message}</p>
+    <div className="flash-message" onClick={() => setOutro(1)} onAnimationEnd={() => setFlashMessage(null)} outro={outro}>
+      <div className={`flash-header-${type}`} />
+      <div className="flash-content">
+        <p>{message}</p>
+      </div>
     </div>
   )
 }
