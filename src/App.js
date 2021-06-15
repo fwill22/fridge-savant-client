e298d9a33972e3b829f734a4cd6efc2888252d6a
ryanpierre <info@ryanpierre.com>
2021-06-15 17:02:20 +0100

    add token based authentication example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import Pages from './pages';
-import './App.css';
+import React from "react";
+import Pages from "./pages";
+import "./App.css";
+import AuthProvider from "./providers/auth";
 
 const App = () => {
   return (
-    <div className='App'>
-      <Pages />
+    <div className="App">
+      <AuthProvider>
+        <Pages />
+      </AuthProvider>
     </div>
   );
 };
