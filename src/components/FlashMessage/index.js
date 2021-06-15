bf7f9bbebb4b53b518e5b226fa02bf0f54c37aed
Kane <kgin888@gmail.com>
2021-06-15 19:29:54 +0100

    outro animation for flash messages completed

--- src/components/FlashMessage/index.js
+++ src/components/FlashMessage/index.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import './index.css';
 
-const FlashMessage = ({type, message, setFlashMessage }) => {
+const FlashMessage = ({type, message, setFlashMessage, resetMessage, outro, setOutro }) => {
 
-  const [outro, setOutro] = useState(0)
+  // const [outro, setOutro] = useState(0)
 
-  const resetMessage = () => {
-    if(outro === 1) {
-      setFlashMessage(null)
-      setOutro(0)
-    }
-  }
+  // const resetMessage = () => {
+  //   if(outro === 1) {
+  //     setFlashMessage(null)
+  //     setOutro(0)
+  //   }
+  // }
 
   return (
     <div className="flash-message" onClick={() => setOutro(1)} onAnimationEnd={() => resetMessage()} outro={outro}>
--- src/providers/Flash/index.js
+++ src/providers/Flash/index.js
@@ -1,22 +1,37 @@
 import React, { useState } from 'react'
 import FlashMessage from '../../components/FlashMessage'
+import './index.css';
 
 export const FlashContext = React.createContext(null);
 
 const FlashProvider = ({ children }) => {
   const [flashMessage, setFlashMessage] = useState(null);
+  const [outro, setOutro] = useState(0);
 
   const createFlashMessage = ({ type, message }) => {
     setFlashMessage({ type, message });
     setTimeout(() => {
-      setFlashMessage(null);
+      setOutro(1)
+      resetMessage()
     }, 10000);
   };
+
+  const resetMessage = () => {
+    if(outro === 1) {
+      setFlashMessage(null)
+      setOutro(0)
+    }
+  }
 
   return (
     <FlashContext.Provider value={{ createFlashMessage }}>
       {flashMessage && (
-        <FlashMessage type={flashMessage.type} message={flashMessage.message} setFlashMessage={setFlashMessage}/>
+        <div className="flash-message" onClick={() => setOutro(1)} onAnimationEnd={() => resetMessage()} outro={outro}>
+          <div className={`flash-header-${flashMessage.type}`} />
+          <div className="flash-content">
+          <p>{flashMessage.message}</p>
+          </div>
+        </div>
       )}
       {children}
     </FlashContext.Provider>
