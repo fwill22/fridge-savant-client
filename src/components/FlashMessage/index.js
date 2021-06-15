b6604093d5e8e14b59f44cf327d099d20dda227b
Kane <kgin888@gmail.com>
2021-06-15 19:18:06 +0100

    created outro animation for click - need to add for timeout

--- src/components/FlashMessage/index.js
+++ src/components/FlashMessage/index.js
@@ -5,8 +5,15 @@ const FlashMessage = ({type, message, setFlashMessage }) => {
 
   const [outro, setOutro] = useState(0)
 
+  const resetMessage = () => {
+    if(outro === 1) {
+      setFlashMessage(null)
+      setOutro(0)
+    }
+  }
+
   return (
-    <div className="flash-message" onClick={() => setOutro(1)} onAnimationEnd={() => setFlashMessage(null)} outro={outro}>
+    <div className="flash-message" onClick={() => setOutro(1)} onAnimationEnd={() => resetMessage()} outro={outro}>
       <div className={`flash-header-${type}`} />
       <div className="flash-content">
         <p>{message}</p>
