3fe60c8fcd7f85c1c41043b1989892445fe727b7
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-06-16 09:55:42 +0100

    Whatsapp button props corrected and index.css files removed as imports from share components

--- src/components/WhatsappShare/index.js
+++ src/components/WhatsappShare/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import './index.css';
 import {
   WhatsappShareButton,
   WhatsappIcon
 } from "react-share";
 
-const WhatsappShare = ({recipeUrl}) => {
+const WhatsappShare = ({title, recipeUrl}) => {
   return (
     <div>    
       <div className="whatsappShare">
         <WhatsappShareButton className="whatsappShareButton"
-          url={recipeUrl}
-          quote={`Hey! I found this great recipe on my Fridge Savant, come check it out!     
-                  To find other great recipes sign up to Fridge Savant here:       
-                  www.FridgeSavant.com`} >
+          title={title}
+          separator=" "
+          url={recipeUrl}>
           <WhatsappIcon size={32} round />
         </WhatsappShareButton>
       </div>
