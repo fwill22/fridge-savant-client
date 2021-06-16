2ed75c941118fe3270bdb0bca2fc4d79dd5a6847
Kane <kgin888@gmail.com>
2021-06-16 15:14:09 +0100

    added carousel imgs locally

--- src/components/signUpForm/index.js
+++ src/components/signUpForm/index.js
@@ -48,7 +48,7 @@ const SignUpForm = ({ handleCardFlip }) => {
       }
 
       try {
-        const response = await fetch('http://localhost:5000/api/users', {
+        const response = await fetch('http://localhost:5000/api/users/signup', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json' 
